f3fd7b769d04c6ca64a339ddc77046579ff33b59
Yamraaj <69627019+crazy-racer@users.noreply.github.com>
2022-07-19 01:43:04 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -234,11 +234,11 @@ def is_url(url: str):
 def is_gdrive_link(url: str):
     return "drive.google.com" in url
 
-def is_gdtot_link(url: str):
+def is_appdrive_link(url: str):
     url = re.match(r'https?://(?:\S*\.)?(?:gdtot)\.\S+', url)
     return bool(url)
 
-def is_appdrive_link(url: str):
+def is_gdtot_link(url: str):
     url = re.match(r'https?://(?:\S*\.)?(?:appdrive|driveapp)\.in/\S+', url)
 
 def is_mega_link(url: str):
