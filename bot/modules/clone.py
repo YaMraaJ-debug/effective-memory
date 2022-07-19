95db4870c4c200dcf8d6ce3b5c9a450e0eaae535
Yamraaj <69627019+crazy-racer@users.noreply.github.com>
2022-07-19 08:12:41 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -5,7 +5,7 @@ from threading import Thread
 from time import sleep
 
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.ext_utils.bot_utils import new_thread, is_gdrive_link, is_appdrive_link, is_gdtot_link
+from bot.helper.ext_utils.bot_utils import new_thread, is_gdrive_link, is_gdtot_link
 from bot.helper.ext_utils.parser import appdrive, gdtot
 from bot.helper.ext_utils.exceptions import DDLException
 from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, deleteMessage, delete_all_messages, update_all_messages, sendStatusMessage
@@ -37,15 +37,11 @@ def _clone(message, bot, multi=0):
         else:
             tag = reply_to.from_user.mention_html(reply_to.from_user.first_name)
         try:
-        is_appdrive = is_appdrive_link(link)
-    is_gdtot = is_gdtot_link(link)
-    if (is_appdrive or is_gdtot):
+        is_gdtot = is_gdtot_link(link)
+    if is_gdtot:
         try:
             msg = sendMessage(f"<b>Processing:</b> <code>{link}</code>", context.bot, update)
             LOGGER.info(f"Processing: {link}")
-            if is_appdrive:
-                appdict = appdrive(link)
-                link = appdict.get('gdrive_link')
             if is_gdtot:
                 link = gdtot(link)
             deleteMessage(context.bot, msg)
@@ -105,10 +101,6 @@ def _clone(message, bot, multi=0):
         if is_gdtot:
             LOGGER.info(f"Deleting: {link}")
             gd.deleteFile(link)
-        if is_appdrive:
-            elif appdict.get('link_type') == 'login':
-                LOGGER.info(f"Deleting: {link}")
-                gd.deleteFile(link)
     else:
         sendMessage('Send Gdrive or gdtot link along with command or by replying to the link by command', bot, message)
 
