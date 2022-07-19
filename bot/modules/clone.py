662211936c9627158ca1e276789f36c06175db8d
Yamraaj <69627019+crazy-racer@users.noreply.github.com>
2022-07-19 11:34:47 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -37,7 +37,7 @@ def _clone(message, bot, multi=0):
         else:
             tag = reply_to.from_user.mention_html(reply_to.from_user.first_name)
     is_gdtot = is_gdtot_link(link)
-    if is_gdtot:
+    if any([is_gdtot]):
         try:
             msg = sendMessage(f"<b>Processing:</b> <code>{link}</code>", context.bot, update)
             LOGGER.info(f"Processing: {link}")
